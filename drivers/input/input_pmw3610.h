14b18c5252d177710dc569e42541c4b143edd80a
riuwich <162534604+riuwich@users.noreply.github.com>
2025-07-17 19:54:22 +0900

    Update input_pmw3610.h

--- drivers/input/input_pmw3610.h
+++ drivers/input/input_pmw3610.h
@@ -23,4 +23,13 @@ int pmw3610_set_resolution(const struct device *dev, uint16_t res_cpi);
  */
 int pmw3610_force_awake(const struct device *dev, bool enable);
 
+static inline int32_t sign_extend(uint32_t value, uint8_t index)
+{
+	__ASSERT_NO_MSG(index <= 31);
+
+	uint8_t shift = 31 - index;
+
+	return (int32_t)(value << shift) >> shift;
+}
+
 #endif /* ZEPHYR_INCLUDE_INPUT_PMW3610_H_ */
